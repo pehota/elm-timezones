03058d313389de0c2ba914e507e985e8c0285560
Dimitar Apostolov <>
2018-03-12 23:39:16 +0100

    Update updater scripts

---
+++ updater/db.js
@@ -0,0 +1,3 @@
+module.exports = {};
+module.exports.checkForDBUpdates = () => Promise.reject('checkForDBUpdates is not implemented yet');
+module.exports.downloadDB = () => Promise.reject('downloadDB is not implemented yet');
