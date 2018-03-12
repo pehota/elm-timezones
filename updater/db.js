b191c2d03a500cd565040ba80a97e547a7d31722
Dimitar Apostolov <>
2018-03-13 00:06:21 +0100

    WIP: Add updater tests

--- updater/db.js
+++ updater/db.js
@@ -1,3 +1,5 @@
 module.exports = {};
-module.exports.checkForDBUpdates = () => Promise.reject('checkForDBUpdates is not implemented yet');
-module.exports.downloadDB = () => Promise.reject('downloadDB is not implemented yet');
+module.exports.checkForDBUpdates = () =>
+  Promise.reject('checkForDBUpdates is not implemented yet');
+module.exports.downloadDB = () =>
+  Promise.reject('downloadDB is not implemented yet');
